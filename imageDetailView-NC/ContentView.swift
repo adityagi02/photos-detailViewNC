9c437a92956d1090439b1161150ae8f692142357
Aditya Tyagi <adityatyagi20bcs126@iiitkottayam.ac.in>
2023-09-23 12:26:34 +0530

    Initial Commit

---
+++ imageDetailView-NC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  imageDetailView-NC
+//
+//  Created by Aditya Tyagi  on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ imageDetailView-NC/imageDetailView_NCApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageDetailView_NCApp.swift
+//  imageDetailView-NC
+//
+//  Created by Aditya Tyagi  on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct imageDetailView_NCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
