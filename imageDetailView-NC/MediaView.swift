4ef6d963b8a555894b3de3b6208bd3d70e709511
adityagi02 <adityagi02@yahoo.com>
2023-10-21 22:27:00 +0530

    Added mediaPreview View

---
+++ imageDetailView-NC/MediaView.swift
@@ -0,0 +1,37 @@
+//
+//  MediaView.swift
+//  imageDetailView-NC
+//
+//  Created by Aditya Tyagi on 21/10/23.
+//
+
+import SwiftUI
+
+struct MediaView: View {
+    var body: some View {
+        ZStack{
+            media().body
+               
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct MediaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaView()
+    }
+}
+
+
+struct media : View {
+    var body: some View {
+        GeometryReader { geometry in
+            Image("placeholder")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                //.background(.red)
+        }
+    }
+}
