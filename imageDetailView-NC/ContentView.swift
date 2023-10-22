d4eb29a8ac32c4f82aa66a3ec70a22d45e5fcdaf
adityagi02 <adityagi02@yahoo.com>
2023-10-22 10:13:28 +0530

    Added Navigation bar UI + sheet call with button

--- imageDetailView-NC/ContentView.swift
+++ imageDetailView-NC/ContentView.swift
@@ -14,56 +14,77 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack{
+            ZStack{
                 MediaView()
                 
+                
                 VStack{
-//
-//                    Button(action: {
-//                        self.showMapSetting = !showMapSetting
-//                    }, label: {
-//                        // Image(systemName: showMapSetting ? "info.fill" : "info" )
-//                        Text("Details")
-//                    })
-//                    //                    .navigationBarTitle("Bottom Sheet")
-//                    .navigationBarItems(
-//                        leading: Button(action: { self.showMapSetting = true }) { Text("Setting") },
-//                        trailing: Button(action: { self.showList = true }) { Text("List") }
-//                    )
+                    //
+                    //                    Button(action: {
+                    //                        self.showMapSetting = !showMapSetting
+                    //                    }, label: {
+                    //                        // Image(systemName: showMapSetting ? "info.fill" : "info" )
+                    //                        Text("Details")
+                    //                    })
+                    //                    //                    .navigationBarTitle("Bottom Sheet")
+                    //                    .navigationBarItems(
+                    //                        leading: Button(action: { self.showMapSetting = true }) { Text("Setting") },
+                    //                        trailing: Button(action: { self.showList = true }) { Text("List") }
+                    //                    )
                 }
+                .bottomSheet(
+                    isPresented: $showMapSetting,
+                    height: 450,
+                    topBarHeight: 16,
+                    topBarCornerRadius: 16,
+                    showTopIndicator: false
+                ) {
+                    mediaDetailView(time: Date(), imageName: "IMG_002", deviceName: "Apple iPhone 12 Pro", imageExtension: "Hief", cameraMode: "Front Camera", apertureValue: 23, lensLength: 2.2, iso: 40, megaPixels: 12, height: 4032, width: 3024, imageSize: 2.3, exposureValue: 0, shutterSpeedApex: 124, longitude: Int32(-116.166_868), latitude: Int32(34.011_286), location: "Delhi, India",  dismiss: { self.showMapSetting = false })
+                }
+                
             }.navigationBarTitle("IMG_002", displayMode: .inline)
-                .navigationBarItems(trailing:
-                                        HStack {
-                    Button(action: {
-                        self.showMapSetting = !showMapSetting
-                    }) {
-                        Image(showMapSetting == false ? "ellipsis.circle" : "ellipsis.circle.fill")
-                            .resizable()
-                            .frame(width: 12)
-                            .foregroundColor(.black)
-                            .listStyle(PlainListStyle())
-                            .bottomSheet(
-                                isPresented: $showMapSetting,
-                                height: 450,
-                                topBarHeight: 16,
-                                topBarCornerRadius: 16,
-                                showTopIndicator: false
-                            ) {
-                                mediaDetailView(time: Date(), imageName: "IMG_002", deviceName: "Apple iPhone 12 Pro", imageExtension: "Hief", cameraMode: "Front Camera", apertureValue: 23, lensLength: 2.2, iso: 40, megaPixels: 12, height: 4032, width: 3024, imageSize: 2.3, exposureValue: 0, shutterSpeedApex: 124, longitude: Int32(-116.166_868), latitude: Int32(34.011_286), location: "Delhi, India",  dismiss: { self.showMapSetting = false })
-                            }
-
+                .toolbar {
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        navigationBar
                     }
-                    Button(action: {
-                        // Action for the second button
-                    }) {
-                        Image("info.circle")
-                            .resizable()
-                            .frame(width: 12)
-                            .foregroundColor(.black)
-                    }
-                }.padding()
-                )
+                }
         }
+    }
+    
+    
+    @ViewBuilder
+    var navBar : some View {
+        HStack{
+            Image(systemName: showMapSetting == false ? "info.circle" : "info.circle.fill")
+                .resizable()
+//                .frame(width: 12)
+//                .foregroundColor(.black)
+                .listStyle(PlainListStyle())
+        }
+    }
+    
+    
+    @ViewBuilder
+    var navigationBar : some View {
+        HStack(spacing: 10) {
+            
+            Button(action: {
+                self.showMapSetting = !showMapSetting
+            }) {
+                navBar
+            }
+            
+            Button(action: {
+                // Action for the second button -> Open Menu Sheet
+            }) {
+                Image(systemName: "ellipsis.circle")
+                .resizable()
+//                .frame(width: 12)
+//                    .foregroundColor(.black)
+            }
+            
+        }.padding(.trailing, 8)
     }
 }
 
--- imageDetailView-NC/MediaView.swift
+++ imageDetailView-NC/MediaView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MediaView: View {
     var body: some View {
-        ZStack{
+        VStack{
             media().body
                
         }
